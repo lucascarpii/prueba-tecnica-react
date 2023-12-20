538b359da8901a96670dd33aa93341d013d00557
Lucas Fuentes <lucascarpii2002@gmail.com>
2023-12-19 21:11:10 -0300

    <feat> Crear vista de sign-in

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -6,13 +6,64 @@ const Navbar = () => {
   const context = useContext(ShoppingCartContext)
   const activeStyle = 'underline underline-offset-4 capitalize'
 
+  // Sign Out
+  const signOut = localStorage.getItem('sign-out')
+  const parsedSignOut = JSON.parse(signOut)
+  const isUserSignOut = context.signOut || parsedSignOut
+
   const handleSignOut = () => {
     const stringifiedSignOut = JSON.stringify(true)
     localStorage.setItem('sign-out', stringifiedSignOut)
     context.setSignOut(true)
   }
 
-  return(
+  const renderView = () => {
+
+    if (isUserSignOut) {
+      return (
+        <li>
+          <NavLink
+            to='/sign-in'
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }
+            onClick={() => handleSignOut()}>
+            Sign out
+          </NavLink>
+        </li>
+      )
+    } else {
+      return (
+        <>
+          <li className="text-black/60">
+            lucascarpii2002@gmail.com
+          </li>
+          <li>
+            <NavLink to={'/my-orders'} className={({ isActive }) => isActive ? activeStyle : undefined}>
+              My Orders
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={'/my-account'} className={({ isActive }) => isActive ? activeStyle : undefined}>
+              My Account
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='/sign-in'
+              className={({ isActive }) =>
+                isActive ? activeStyle : undefined
+              }
+              onClick={() => handleSignOut()}>
+              Sign out
+            </NavLink>
+          </li>
+        </>
+      )
+    }
+  }
+
+  return (
     <nav className="flex bg-white justify-between items-center fixed top-0 z-10 w-full py-5 px-8 text-base font-normal font-pop">
       <ul className="flex items-center gap-3">
         <li className="font-bold text-lg">
@@ -21,61 +72,39 @@ const Navbar = () => {
           </NavLink>
         </li>
         <li>
-          <NavLink 
+          <NavLink
             to={'/'}
             onClick={() => context.setSearchByCategory('')}
-            className={({isActive})=> isActive ? activeStyle : undefined}>
+            className={({ isActive }) => isActive ? activeStyle : undefined}>
             All
           </NavLink>
         </li>
         {
           context.categories?.map(category => (
-          <li key={category.id}>
-            <NavLink 
-              to={`/category/${category.name.toLowerCase()}`}
-              onClick={() => context.setSearchByCategory(category.name)}
-              className={({isActive})=> isActive ? activeStyle : 'capitalize'}>
-              {category.name}
-            </NavLink>
-          </li>
+            <li key={category.id}>
+              <NavLink
+                to={`/category/${category.name.toLowerCase()}`}
+                onClick={() => context.setSearchByCategory(category.name)}
+                className={({ isActive }) => isActive ? activeStyle : 'capitalize'}>
+                {category.name}
+              </NavLink>
+            </li>
           ))
         }
       </ul>
       <ul className="flex items-center gap-3">
-        <li className="text-black/60">
-          lucascarpii2002@gmail.com
-        </li>
-        <li>
-          <NavLink to={'/my-orders'} className={({isActive})=> isActive ? activeStyle : undefined}>
-            My Orders
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={'/my-account'} className={({isActive})=> isActive ? activeStyle : undefined}>
-            My Account
-          </NavLink>
-        </li>
-        <li>
-        <NavLink
-            to='/sing-in'
-            className={({ isActive }) =>
-              isActive ? activeStyle : undefined
-            }
-            onClick={() => handleSignOut()}>
-            Sign out
-          </NavLink>
-        </li>
+        {renderView()}
         <li className="flex items-center justify-center gap-1">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-            <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
-          </svg>
-          <div className="text-md leading-none text">
-          {context.cartProducts.length}
-          </div>
-        </li>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+              <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+            </svg>
+            <div className="text-md leading-none text">
+              {context.cartProducts.length}
+            </div>
+          </li>
       </ul>
     </nav>
   )
 }
 
-export {Navbar}
+export { Navbar }
--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,11 +1,35 @@
+import { Link } from "react-router-dom"
 import { Layout } from "../../Components/Layout"
 
 function SignIn() {
   return (
     <Layout>
-    <h1>SignIn</h1>
+      <h1 className="font-semibold text-2xl text-center mb-6 w-80">Welcome</h1>
+      <div className="flex flex-col w-80">
+        <p className="flex flex-col mb-4">
+          <span className="font-base text-sm">Email: </span>
+          <span className="p-2 border rounded-lg">example@gmail.com</span>
+        </p>
+        <p className="flex flex-col mb-4">
+          <span className="font-base text-sm">Password: </span>
+          <span className="p-2 border rounded-lg">********</span>
+        </p>
+        <Link to="/">
+          <button className="bg-black disabled:bg-black/40 text-white font-medium w-full rounded-lg py-3 mt-4 mb-2">
+            Log In
+          </button>
+        </Link>
+        <div className="text-center">
+          <a className="font-base text-xs underline underline-offset-4" href="/">
+            Forgot my password
+          </a>
+        </div>
+        <button className="bg-black disabled:bg-black/40 text-white font-medium w-full rounded-lg mt-6 py-3">
+          Sign Up
+        </button>
+      </div>
     </Layout>
   )
 }
 
-export {SignIn}
+export { SignIn }
