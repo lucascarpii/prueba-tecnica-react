b5fc24d8f0e99d6e7805ee94485a390cdeb8c825
Lucas Fuentes <lucascarpii2002@gmail.com>
2023-12-19 23:54:40 -0300

    <feat> Agregar estado local para mostrar la vista de crear cuenta o de hacer log in

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom"
 import { Layout } from "../../Components/Layout"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { ShoppingCartContext } from "../../Context"
 
 function SignIn() {
   const context = useContext(ShoppingCartContext)
+  const [view, setView] = useState('user-info')
 
   // Account
   const account = localStorage.getItem('account')
@@ -14,9 +15,8 @@ function SignIn() {
   const noAccountInLocaleState = context.account ? Object.keys(context.account).length === 0 : true
   const hasUserAnAccount = !noAccountInLocaleStorage || !noAccountInLocaleState
 
-  return (
-    <Layout>
-      <h1 className="font-semibold text-2xl text-center mb-6 w-80">Welcome</h1>
+  const renderLogin = () => {
+    return (
       <div className="flex flex-col w-80">
         <p className="flex flex-col mb-4">
           <span className="font-base text-sm">Email: </span>
@@ -27,7 +27,7 @@ function SignIn() {
           <span className="p-2 border rounded-lg min-h-[42px]">{parsedAccount?.password}</span>
         </p>
         <Link to="/">
-          <button 
+          <button
             className="bg-black disabled:bg-black/40 text-white font-medium w-full rounded-lg py-3 mt-4 mb-2"
             disabled={!hasUserAnAccount}
           >
@@ -39,13 +39,28 @@ function SignIn() {
             Forgot my password
           </a>
         </div>
-        <button 
-          className="bg-black disabled:bg-black/40 text-white font-medium w-full rounded-lg mt-6 py-3"
-          disabled={hasUserAnAccount}
-        >
-          Sign Up
+        <button
+          className='border-2 font-medium border-black disabled:text-black/40 disabled:border-black/40 rounded-lg mt-6 py-3 transition-all hover:-translate-y-0.5'
+          onClick={() => setView('create-user-info')}
+          disabled={hasUserAnAccount}>
+          Sign up
         </button>
       </div>
+    )
+  }
+
+  const renderCreateUserInfo = () => {
+    return (
+      <></>
+    )
+  }
+
+  const renderView = () => view === 'create-user-info' ? renderCreateUserInfo() : renderLogin()
+
+  return (
+    <Layout>
+      <h1 className="font-semibold text-2xl text-center mb-6 w-80">Welcome</h1>
+      {renderView()}
     </Layout>
   )
 }
